b98b18c1251f6e51cb6d08122e12eb59a5f1fd28
Felipe Borges <felipetecacc@gmail.com>
2024-07-13 16:14:34 -0300

    first commit

---
+++ src/main/java/com/devinc/planner/participant/ParticipantCreateResponse.java
@@ -0,0 +1,6 @@
+package com.devinc.planner.participant;
+
+import java.util.UUID;
+
+public record ParticipantCreateResponse(UUID id) {
+}
---
+++ src/main/java/com/devinc/planner/participant/ParticipantRequestPayload.java
@@ -0,0 +1,4 @@
+package com.devinc.planner.participant;
+
+public record ParticipantRequestPayload(String name, String email) {
+}
